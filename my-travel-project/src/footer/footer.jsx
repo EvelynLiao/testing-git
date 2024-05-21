a74d9078e0584a75801d525eec9372ce4790e891
TayebaN <tayebe.naseri.tn@gmail.com>
2024-05-21 15:14:09 +0200

    add h1

---
+++ my-travel-project/src/footer/footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+//import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <h1>hello</h1>
+      <p>
+        &copy; {new Date().getFullYear()} Your Company. All rights reserved.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
